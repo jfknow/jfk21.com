a16a0fb2e87a411ed0ee3a54f52d3e60e8540421
jfkdidit <web@jfk21.com>
2016-07-19 10:25:46 +0200

    Update analytics.js

--- content/lib/analytics.js
+++ content/lib/analytics.js
@@ -1,4 +1,4 @@
-
+// Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -6,3 +6,15 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-80903842-2', 'auto');
 ga('send', 'pageview');
+
+
+// insert border
+var body = document.getElementsByTagName("body")[0];
+
+var div = document.createElement("div");
+div.id = "border-top"
+body.insertBefore(div, body.firstChild);
+
+div = document.createElement("div");
+div.id = "border-bottom"
+body.insertBefore(div, body.firstChild);
